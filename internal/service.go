198229cb323949550e27d907c4d91e17fd9855d1
Mario Canalella <marcanalella@gmail.com>
2024-02-14 23:24:32 +0100

    fix scheduler

--- internal/service.go
+++ internal/service.go
@@ -163,7 +163,7 @@ func GetEmojiSemaphore(impact string) string {
 func (s service) ScheduledNotification(recipients []int) {
 	var message string
 	s1 := gocron.NewScheduler(time.UTC)
-	_, err := s1.Every(1).Day().At("17:00").Do(func() { //US Virginia 17 -> italia 23
+	_, err := s1.Every(1).Day().At("22:00").Do(func() {
 		events, err := s.GetEconomicCalendarForNextDay()
 		if err != nil {
 			log.Printf("got error when calling Economic Calendar API %s", err.Error())
@@ -219,7 +219,7 @@ func (s service) ScheduledNotification(recipients []int) {
 func (s service) Readyz(recipients []int) {
 	var message string
 	s2 := gocron.NewScheduler(time.UTC)
-	_, err := s2.Every(1).Day().At("16:59").Do(func() { //US Virginia 17 -> italia 23
+	_, err := s2.Every(1).Day().At("21:59").Do(func() {
 		if time.Now().Weekday() != 6 {
 			message = "EconomicCalendarAndNewsBot Running " + emoji.BeamingFaceWithSmilingEyes.String()
 			for _, chatId := range recipients {
